b7792f4c120db20dafd85f9310fe86ed15cbb595
tanhimahmed <tanhimahmed13@gmail.com>
2024-03-13 20:36:55 -0400

    session 3

--- src/main/java/dev/tanhim/App.java
+++ src/main/java/dev/tanhim/App.java
@@ -22,6 +22,8 @@ public class App
     }
 
     // leetcode 27
+    // int[] nums = { 3,2,2,3, 0, 1, 34, 2, 3 };
+    // int[] nums2 = { 3,2,2,3 };
     public static int removeElement(int[] nums, int val) {
         if (nums.length == 0) {
             return 0;
@@ -36,6 +38,14 @@ public class App
         }
         Arrays.sort(nums);
         return k;
+    }
+
+    // leetcode 50
+    public static boolean canJump(int[] nums) {
+        
+
+
+        return false;
     }
 
     // leetcode 80
@@ -59,6 +69,8 @@ public class App
     }
 
     // leetcode 88
+    // Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+    // Output: [1,2,2,3,5,6]
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
         int index = 0;
         for (int i=m; i < m+n; i++) {
@@ -69,6 +81,8 @@ public class App
     }
 
     // leetcode 121  ---> my solution
+    // Input: prices = [7,1,5,3,6,4]
+    // Output: 5
     public static int maxProfit(int[] prices) {
         int maxProfit = 0;
         
@@ -87,6 +101,8 @@ public class App
     }
 
     // leetcode 121 ---> optimal solition
+    // Input: prices = [7,1,5,3,6,4]
+    // Output: 5
     public static int maxProfitOptimal(int[] prices) {
         int maxProfit = 0;
         int left = 0;
@@ -106,6 +122,8 @@ public class App
     }
 
     // leetcode 122
+    // Input: prices = [7,1,5,3,6,4]
+    // Output: 7
     public static int maxProfitMultipleTrades(int[] prices) {
         int left = 0;
         int right = 1;
@@ -130,6 +148,34 @@ public class App
         int mid = nums.length / 2;
         return nums[mid];
     }
+
+    // leetcode 189
+    // Input: nums = [1,2,3,4,5,6,7], k = 3
+    // Output: [5,6,7,1,2,3,4]
+    public static void rotate(int[] nums, int k) {        
+        k = k % nums.length;
+        if (k < 0) {
+            k += nums.length;
+        }  
+        reverse(nums, 0 , nums.length - k -1);
+        reverse(nums, nums.length - k, nums.length - 1);
+        reverse(nums, 0, nums.length - 1);
+
+    }
+    public static void reverse(int[] nums, int i, int j) {
+        int left = i;
+        int right = j;
+
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums [right];
+            nums[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
 
     public static void main( String[] args )
     {
@@ -157,14 +203,11 @@ public class App
         // Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
         // Output: [1,2,2,3,5,6]
         //
-        int[] nums1 = { 1,2,3,0,0,0 };
-        int[] nums2 = { 2,5,6 };
-        merge(nums1, 3, nums2, 3);
-
-        System.out.println(Arrays.toString(nums1));
-
+        // int[] nums1 = { 1,2,3,0,0,0 };
+        // int[] nums2 = { 2,5,6 };
+        // merge(nums1, 3, nums2, 3);
+        // System.out.println(Arrays.toString(nums1));
         
-
         // ===================================================
         // 121)
         // Input: prices = [7,1,5,3,6,4]
